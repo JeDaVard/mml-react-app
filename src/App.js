b246e37e5035a0407e4a2aa0ad9bda3c3ce27f5f
Davit Vardanyan <jedavard@gmail.com>
2020-05-30 05:04:20 +0400

    file type fixes

--- src/App.js
+++ src/App.js
@@ -36,7 +36,8 @@ function App(props) {
     const postVideo = e => {
         e.preventDefault();
 
-        // if (!uploadFile || uploadFile.type.toLowerCase() !== 'video/mp4') return console.log('Only mp4 allowed');
+        if (!uploadFile || !uploadFile.type.toLowerCase().startsWith('video')) return console.log('Only valid videos allowed');
+
         setData(state => ({...state, loading: true}))
         const video = new FormData();
         video.append('file', uploadFile)
